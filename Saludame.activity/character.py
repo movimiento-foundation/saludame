14b952e11bc9651f581d678d9e04dffed44916ff
echinelli <emilianochinelli@gmail.com>
2010-10-08 13:09:39 -0200

    Changes in modules: character.py, character_creator.py, app_init.py
    *remove class CharacterManager
    *add methods to class Character
    *implemented CharacterLoader

--- Saludame.activity/character.py
+++ Saludame.activity/character.py
@@ -1,41 +1,44 @@
 # -*- coding: utf-8 -*-
 
 import actions
+import pygame
 
-class CharacterManager:
-    
-    def __init__(self, character, actions_dictionary, places_dictionary):
-        self.character = character
-        self.actions_dictionary = actions_dictionary
-        self.places_dictionary = places_dictionary
-    
-    def set_character_place(self, place_id):
-        self.character.set_place(place_id)
-    
-    def action_perform(self, action_id):
-        place = self.places_dictionary[self.character.actual_place] #lugar donde se ecuentra el kid actualmente
-        if(place.allowed_action(action_id)):#continúa con la acción, solo si es permitida en el lugar
-            action = self.actions_dictionary[action_id]
-            action.perform()
         
 class Character:
     
-    def __init__(self, name, level, score, hair_color, eyes_color, skin_color, shoes_color, status_bar_list):
+    def __init__(self, name, level, score, hair_color, eyes_color, skin_color, shoes_color, status_bar_list, clothes, places_dictionary, actions_dictionary):
         self.name = name
         self.level = level
         self.score = score
+        
+        self.actions_dictionary = actions_dictionary
+        self.places_dictionary = places_dictionary
+        """         """
         
         """ visuals """
         self.hair_color = hair_color
         self.eyes_color = eyes_color
         self.skin_color = skin_color
         self.shoes_color = shoes_color
+        self.clothes = clothes
         """ """
         self.actual_place = None
         self.status_bar_list = status_bar_list #status bars
         self.active_events_list = []
 
         self.mood_list = [] #Class Mood instance's list
+        
+    def action_perform(self, action_id):
+        place = self.places_dictionary[self.actual_place] #lugar donde se ecuentra el kid actualmente
+        if(place.allowed_action(action_id)):#continúa con la acción, solo si es permitida en el lugar
+            action = self.actions_dictionary[action_id]
+            action.perform()
+    
+    def set_character_place(self, place_id):
+        self.character.set_place(place_id)
+    
+    def set_clothes(self, clothes):
+        self.clothes = clothes
         
     def set_place(self, place_id):
         self.actual_place = place_id
@@ -73,7 +76,7 @@ class Clothes:
     def __init__(self, clothes_id, texture_path, weather_effects_list):
         self.clothes_id = clothes_id
         self.texture_path = texture_path
-        self.weather_effects_list = weather_effects_list #list of tuples (id_weather, effect_indoor, effect_outdoiir
+        self.weather_effects_list = weather_effects_list #list of tuples (id_weather, effect_indoor, effect_outdoor)
         
         
         
