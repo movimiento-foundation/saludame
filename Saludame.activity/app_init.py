45bf5a2f8ab4a416c8c3f8850c4da6daedeef3df
echinelli <emilianochinelli@gmail.com>
2010-10-14 10:13:00 -0200

    fixes up on modules character_creator app_init

--- Saludame.activity/app_init.py
+++ Saludame.activity/app_init.py
@@ -14,7 +14,54 @@ class AppLoader:
         """          """
         """ actions """
         self.actions_dictionary = actions_loader.get_actions_dictionary() #diccionario {id_actions: Action}
+        # -*- coding: utf-8 -*-
+import menu_creator
+import status_bars_creator
+import actions_creator
+import character_creator
+
+class AppLoader:
+
+    def __init__(self):
+        """ loaders """
+        actions_loader = actions_creator.ActionsLoader()
+        bars_loader = status_bars_creator.BarsLoader()
+
+        """          """
+        """ actions """
+        self.actions_dictionary = actions_loader.get_actions_dictionary() #diccionario {id_actions: Action}
         
+        """ status bars """
+        self.character_bars = bars_loader.get_third_level_bars() #the third level status bars
+
+        
+        """ places """
+        character_loader = character_creator.CharacterLoader(self.actions_dictionary, self.character_bars)
+        self.places_dictionary = character_loader.get_places_dictionary()
+        
+        """ character """
+        self.character = character_loader.get_character()
+        
+        """ menu """
+        self.menu = menu_creator.load_menu(self.character)
+        """ visuals """
+    
+        
+    def get_character(self):
+        return self.character
+    
+    def get_menu(self):
+        return self.menu
+    
+    def get_actions(self):
+        return self.actions_dictionary
+    
+
+    
+    
+    
+
+
         """ status bars """
         self.character_bars = bars_loader.get_third_level_bars() #the third level status bars
         
