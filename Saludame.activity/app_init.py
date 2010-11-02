0a2b1642608af8e164ae9c06ed675ca3f04692c3
echinelli <emilianochinelli@gmail.com>
2010-11-02 15:33:35 -0200

    Events, and events handling

--- Saludame.activity/app_init.py
+++ Saludame.activity/app_init.py
@@ -4,6 +4,8 @@ import status_bars_creator
 import actions_creator
 import character_creator
 import game_manager
+import events
+import effects
 
 
 class AppLoader:
@@ -20,7 +22,9 @@ class AppLoader:
         ### status bars 
         self.status_bars_controller = self.bars_loader.get_bar_controller()
         self.character_bars = self.bars_loader.get_third_level_bars() #the third level status bars
-             
+        
+        ### events
+        self.events_list = self.__load_events(self.status_bars_controller)
         ### places
         character_loader = character_creator.CharacterLoader(self.actions_list, self.character_bars)
         self.places_dictionary = character_loader.get_places_dictionary()
@@ -29,7 +33,7 @@ class AppLoader:
         self.character = character_loader.get_character()
         
         ### game manager
-        self.game_man = game_manager.GameManager(self.character, self.status_bars_controller, self.actions_list, None, windows_controller)
+        self.game_man = game_manager.GameManager(self.character, self.status_bars_controller, self.actions_list, self.events_list, None, windows_controller)
         self.game_man.add_background_action("BARS_DEC") #acción de decrementar las barras
         
         ### menu
@@ -53,13 +57,33 @@ class AppLoader:
         return self.menu
     
     def get_actions(self):
-        return self.actions_dictionary
+        return self.actions_list
+    
+    def get_events(self):
+        return self.events_list
 
-
+    
+    def __load_events(self, bars_controller):
+        #(self, picture, name, appereance_probability, time_span, kind, event_status, effect)
+        #temporal para testear eventos
+        dec = -1.0
+        inc = 1.0
+        ef1 = effects.Effect(bars_controller, [("nutrition", dec), ("physica", dec), ("hygiene", dec), ("fun", dec)])
+        ef2 = effects.Effect(bars_controller, [("nutrition", inc), ("physica", inc), ("hygiene", inc), ("fun", inc)])
+        ef3 = effects.Effect(bars_controller, [("fun", 5.0)])
+        ef4 = effects.Effect(bars_controller, [("nutrition", 5.0)])
+        
+        event_dec = events.Event("picture_path", "decremento", 4, 5, "kind", None, ef1)
+        event_inc = events.Event("picture_path", "incremento", 6, 5, "kind", None, ef2)
+        event_fat = events.Event("picture_path", "engordar", 5, 5, "kind", None, ef3)
+        event_happy = events.Event("picture_path", "diversion", 8, 5, "kind", None, ef4)
+        
+        return [event_dec, event_inc, event_fat, event_happy]
 
     
     
     
+
 
 
 
--- Saludame.activity/game_manager.py
+++ Saludame.activity/game_manager.py
@@ -2,13 +2,15 @@
 
 CONTROL_INTERVAL = 15 #cantidad de señales hasta que realiza un checkeo de las acciones.
 
+import random
+
 class GameManager:
     """
     Clase gestora del sistema. Se encarga del control de las acciones
     y los eventos del juego.
     """
     
-    def __init__(self, character, bars_controller, actions_list, places_list, windows_controller):
+    def __init__(self, character, bars_controller, actions_list, events_list, places_list, windows_controller):
         """
         Constructor de la clase
         """
@@ -16,13 +18,20 @@ class GameManager:
         self.bars_controller = bars_controller
         self.count = 0 #sirve como 'clock' interno, para mantener un orden de tiempo dentro de la clase.
         
-        self.actions_list = actions_list 
+        self.events_list = events_list
+        self.actions_list = actions_list
         self.places_list = places_list
         
         self.background_actions = []
         self.active_char_action = None #Active character action, Action instance
         
+        self.active_event = None 
+        
         self.windows_controller = windows_controller
+        
+        #for events handling:
+        self.max_rand = self.__calculate_max_rand(self.events_list)
+        self.probability_ranges = self.__calculate_ranges(self.events_list)
 
     def set_active_action(self, action_id):
         #place = get_place(self.character.actual_place) 
@@ -71,6 +80,7 @@ class GameManager:
         if(self.count >= CONTROL_INTERVAL):
             self.__control_active_actions()
             self.bars_controller.calculate_score()
+            self.__control_active_events()
             self.count = 0
     
     def __control_score(self):
@@ -91,9 +101,6 @@ class GameManager:
                 self.active_char_action.time_left = self.active_char_action.time_span
                 self.active_char_action = None
     
-    def __control_active_events(self):
-        None
-    
     def get_place(self, id_place):
         """
         Returns the place asociated to the id_place
@@ -109,3 +116,57 @@ class GameManager:
         for action in self.actions_list:
             if(action.id == action_id):
                 return action
+            
+## Events handling
+
+    def __control_active_events(self):
+        """
+        Active event handler
+        """
+        if(self.active_event):
+            if(self.active_event.time_left):
+                self.active_event.perform()
+            else:
+                self.active_event.reset()
+                self.active_event = self.__get_new_event()
+                print "se disparó el evento: ", self.active_event.name
+        else:
+            self.active_event = self.__get_new_event()
+            print "se disparó el evento: ", self.active_event.name
+    
+    def __get_new_event(self):
+        """
+        Get a random event
+        """
+        rand = random.randint(0, self.max_rand)
+        for i in range(0, len(self.probability_ranges)):
+            if(rand >= self.probability_ranges[i][0] and rand <= self.probability_ranges[i][1]):
+                return self.events_list[i]
+            
+    def __calculate_max_rand(self, events_list):
+        """
+        Calculates the max random number
+        """
+        max_rand = 0
+        for event in events_list:
+            max_rand += event.appereance_probability
+        return max_rand
+    
+    def __calculate_ranges(self, events_list):
+        """
+        Calculate a probability range for each event and returns
+        a list of ranges
+        """
+        previous = 0
+        ranges = []
+        for event in events_list:
+            ranges += [(previous, previous + event.appereance_probability)]
+            previous += event.appereance_probability + 1
+        return ranges
+    
+    
+    
+    
+    
+    
+
