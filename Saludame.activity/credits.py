63c8758626fff74399c9eee74af11e2598ad2474
flavio <fdanesse@gmail.com>
2019-09-10 22:36:45 -0300

    port credits.py

--- Saludame.activity/credits.py
+++ Saludame.activity/credits.py
@@ -16,29 +16,34 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Saludame. If not, see <http://www.gnu.org/licenses/>.
 
-import gtk
-import rsvg
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('Gdk', '3.0')
+from gi.repository import Gtk
+from gi.repository import Gdk
+from gi.repository import GdkPixbuf
 
-class Credits(gtk.Fixed):
+
+class Credits(Gtk.Fixed):
     
     def __init__(self):
-        gtk.Fixed.__init__(self)
+        
+        Gtk.Fixed.__init__(self)
+
         self.loaded = False
         self.reloaded = False
         
-        self.connect("expose-event", self._expose_cb)
-                
-        self.viewport_initialized = False
-        
+        self.connect("draw", self._expose_cb)
+                        
     def before_show(self):
         if not self.loaded:
-            self.pixbuf = gtk.gdk.pixbuf_new_from_file("credits/background.jpg")
+            self.pixbuf = GdkPixbuf.Pixbuf.new_from_file("credits/background.jpg")
             
-            self.credits_pixbuf = rsvg.Handle(file="credits/saludame.svg").get_pixbuf()
+            self.credits_pixbuf = GdkPixbuf.Pixbuf.new_from_file("credits/saludame.svg")
             
             height = self.credits_pixbuf.get_height()
-            self.adj = gtk.Adjustment(value=0, lower=0, upper=height, step_incr=20, page_incr=200, page_size=550)
-            scroll = gtk.VScrollbar(self.adj)
+            self.adj = Gtk.Adjustment(value=0, lower=0, upper=height, step_incr=20, page_incr=200, page_size=550)
+            scroll = Gtk.VScrollbar(self.adj)
             scroll.set_size_request(40, 550)
             scroll.show()
             scroll.connect("value-changed", self.scrolled)
@@ -48,9 +53,9 @@ class Credits(gtk.Fixed):
             self.loaded = True
             
         elif not self.reloaded:
-            self.credits_pixbuf = rsvg.Handle(file="credits/saludame.svg").get_pixbuf()
+            self.credits_pixbuf = GdkPixbuf.Pixbuf.new_from_file("credits/saludame.svg")
             self.reloaded = True
-        
+
     def ditch(self):
         self.credits_pixbuf = None
         self.reloaded = False
@@ -58,20 +63,25 @@ class Credits(gtk.Fixed):
     def scrolled(self, gtkrange):
         self.queue_draw()
         
-    def _expose_cb(self, widget, event):
-        widget.window.draw_pixbuf(widget.style.bg_gc[gtk.STATE_NORMAL], self.pixbuf, 0, 0, 0, 0)
-        
+    def _expose_cb(self, widget, context):
+        Gdk.cairo_set_source_pixbuf(context, self.pixbuf, 0, 0)
+        context.paint()
+
         y = int(self.adj.get_value())
         width = self.credits_pixbuf.get_width()
         height = 550
-        widget.window.draw_pixbuf(widget.style.bg_gc[gtk.STATE_NORMAL], self.credits_pixbuf, 0, y, 50, 150, width, height)
-        
+
+        Gdk.cairo_set_source_pixbuf(context, self.credits_pixbuf, 0, 0)
+        context.paint()
+
+
 if __name__ == "__main__":
     c = Credits()
     c.before_show()
-    main_window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-    main_window.set_size_request(1200,700)
+    main_window = Gtk.Window(Gtk.WindowType.TOPLEVEL)
+    main_window.maximize()
     main_window.add(c)
     main_window.show_all()
-    gtk.main()
+    main_window.connect("delete-event", Gtk.main_quit)
+    Gtk.main()
     
